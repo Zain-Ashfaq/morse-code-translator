fc86e203c2c26bcbaa1f7c3c7c0000a61b1f79f4
Zain-Ashfaq <zashfaq00@gmail.com>
2022-10-14 13:31:50 +0100

    converts string into morse code (print only)

--- app.js
+++ app.js
@@ -39,22 +39,45 @@ const morseCode = {
   7: "--...",
   8: "---..",
   9: "----.",
+  " ": "/",
 };
-let inputTextValue;
+
+let morseCodeAnsArray = [];
+
 const multiply = (numberOne, numberTwo) => {
   return numberOne * numberTwo;
 };
 
-const printValue = () => {
+const getValue = () => {
   const tempVal = document.getElementById("Enter_sentence").value;
-  inputTextValue = tempVal;
-  console.log(inputTextValue);
+  console.log("this is temp val", tempVal);
+
+  const inputTextValueChars = tempVal.split("");
+  console.log("this is input text value chars", inputTextValueChars);
+
+  translate(inputTextValueChars, morseCode);
 };
+const translate = (sentence, morseCodeObj) => {
+  console.log("this is sentence param", sentence);
+  for (let i = 0; i < sentence.length; i++) {
+    for (arr in Object.keys(morseCodeObj)) {
+      if (Object.keys(morseCodeObj)[arr] == sentence[i]) {
+        console.log("this is morse code obj", Object.values(morseCodeObj)[arr]);
+        morseCodeAnsArray.push(Object.values(morseCodeObj)[arr]);
+      }
+    }
+  }
+  let zain = morseCodeAnsArray;
 
-console.log(morseCode.a);
-console.log(Object.keys(morseCode));
-console.log(Object.values(morseCode));
+  console.log(
+    "this is morse code answer array",
+    zain.join().replaceAll(",", " ")
+  );
+};
+// console.log(morseCode.a);
+// console.log(Object.keys(morseCode));
+// console.log(Object.values(morseCode));
 
-englishToMorseBtn.addEventListener("click", printValue);
+englishToMorseBtn.addEventListener("click", getValue);
 
 //module.exports = { multiply };
