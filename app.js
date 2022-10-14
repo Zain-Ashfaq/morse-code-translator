ccca6c98fa9d88c40cc97e70aabd422f74c3b437
Zain-Ashfaq <zashfaq00@gmail.com>
2022-10-14 12:21:00 +0100

    Updated some tests. Need to write functions

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+// import { morseCodeData } from "./morse_code_info.js";
+const morseCode = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+};
+
 const multiply = (numberOne, numberTwo) => {
   return numberOne * numberTwo;
 };
+
+console.log(morseCode.a);
+console.log(Object.keys(morseCode));
+console.log(Object.values(morseCode));
 
 module.exports = { multiply };
--- morse.spec.js
+++ morse.spec.js
@@ -11,13 +11,28 @@ it("should translate a = *-", () => {
 });
 
 // test for valid character
+it("should match [0-9a-zA-Z] regex code to be valid", () => {
+  const result = checkString("aBc0");
+  expect(result).toBe(true);
+});
 
-// test for string(s) character
 // test for spaces
-// test for a number
-// test for case sensitivity
+it("regex s+ checks for space ", () => {
+  const result = checkSpace("help at charlie");
+  expect(result).toBe("help/at/charlie");
+});
+
 // test for length of dots and/or dashes
 // test for symbol characters?
 // test for combination of different chars
 // test for no input
+it("button clicked without inputting anything", () => {
+  const result = checkInput("");
+  expect(result).toBe("error");
+});
+
 // test for inavlid input
+it("input string contains invalid characters", () => {
+  const result = checkInputString("748%^$£");
+  expect(result).toBe("error");
+});
